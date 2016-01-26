bf893653bf1f0476ca65045d8e3554700e9ab70c
Geetha Yedida <yedida.geetha@gmail.com>
2016-01-26 12:03:55 -0700

    c5

---
+++ history.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+##---(Sat Jan 23 20:43:33 2016)---
+import IPython
+print "IPython version:      %6.6s (need at least 1.0)"
+runfile('C:/Users/Geetha Yedida/.spyder2/temp.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+%clear
+runfile('C:/Users/Geetha Yedida/.spyder2/temp.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+%clear
+
+##---(Sat Jan 23 21:32:51 2016)---
+runfile('C:/Users/Geetha Yedida/.spyder2/temp.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+
+##---(Mon Jan 25 09:45:25 2016)---
+runfile('C:/Users/Geetha Yedida/.spyder2/temp.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+runfile('C:/Users/Geetha Yedida/.spyder2/grade.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+runfile('C:/Users/Geetha Yedida/.spyder2/function_pay.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+runfile('C:/Users/Geetha Yedida/.spyder2/try_except.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+6
+runfile('C:/Users/Geetha Yedida/.spyder2/try_except.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+7
+runfile('C:/Users/Geetha Yedida/.spyder2/try_except.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+%clear
+runfile('C:/Users/Geetha Yedida/.spyder2/try_except.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+%clear
+runfile('C:/Users/Geetha Yedida/.spyder2/try_except.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+runfile('C:/Users/Geetha Yedida/.spyder2/try_except.py', wdir='C:/Users/Geetha Yedida/.spyder2')
+runfile('C:/Users/Geetha Yedida/test.py', wdir='C:/Users/Geetha Yedida')
