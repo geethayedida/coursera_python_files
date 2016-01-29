c9bb53feee7e100500ab21c41fb99bbc268c55bc
Geetha Yedida <yedida.geetha@gmail.com>
2016-01-28 20:28:25 -0700

    web_scraping

---
+++ following_links.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 10:51:06 2016
+
+@author: Geetha Yedida
+"""
+
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter URL: ')
+count = raw_input('Enter count: ')
+position = raw_input('Enter position: ')
+
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
+tag_pos = 0
+tags = soup('a')
+for p in range(int(position)):
+    for tag in tags:
+        tag_pos = tag_pos + 1
+        if (int(tag_pos) == int (count)):
+            url =  tag.get('href', None)
+            print url
+    continue
+# Retrieve all of the anchor tags
