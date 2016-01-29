f0021e3b82181e6f6856484de55d92fbdc06429a
Geetha Yedida <yedida.geetha@gmail.com>
2016-01-28 20:28:51 -0700

    xml

---
+++ extract_from_xml.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 20:19:11 2016
+
+@author: Geetha Yedida
+"""
+
+import urllib
+import xml.etree.ElementTree as ET
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
+
+while True:
+    address = raw_input('Enter location: ')
+    if len(address) < 1 : break
+
+    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
+    print 'Retrieving', url
+    uh = urllib.urlopen(url)
+    data = uh.read()
+    print 'Retrieved',len(data),'characters'
+    print data
+    tree = ET.fromstring(data)
+
+
+    results = tree.findall('result')
+    lat = results[0].find('geometry').find('location').find('lat').text
+    lng = results[0].find('geometry').find('location').find('lng').text
+    location = results[0].find('formatted_address').text
+
+    print 'lat',lat,'lng',lng
+    print location
