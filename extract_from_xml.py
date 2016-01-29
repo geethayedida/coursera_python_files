14abb0af1bec8defdb650333cac101a6cb5cc444
Geetha Yedida <yedida.geetha@gmail.com>
2016-01-29 16:10:15 -0700

    update

--- extract_from_xml.py
+++ extract_from_xml.py
@@ -8,25 +8,12 @@ Created on Thu Jan 28 20:19:11 2016
 import urllib
 import xml.etree.ElementTree as ET
 
-serviceurl = 'http://maps.googleapis.com/maps/api/geocode/xml?'
-
-while True:
-    address = raw_input('Enter location: ')
-    if len(address) < 1 : break
-
-    url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
-    print 'Retrieving', url
-    uh = urllib.urlopen(url)
-    data = uh.read()
-    print 'Retrieved',len(data),'characters'
-    print data
-    tree = ET.fromstring(data)
-
-
-    results = tree.findall('result')
-    lat = results[0].find('geometry').find('location').find('lat').text
-    lng = results[0].find('geometry').find('location').find('lng').text
-    location = results[0].find('formatted_address').text
-
-    print 'lat',lat,'lng',lng
-    print location
+url = raw_input('Enter URL: ')
+html = urllib.urlopen(url).read()
+xml = ET.fromstring(html)
+lst = xml.findall('.//count')
+sum = 0
+print lst
+#for num in range(len(lst)):
+#   sum = sum + int(lst[num])
+#print sum
